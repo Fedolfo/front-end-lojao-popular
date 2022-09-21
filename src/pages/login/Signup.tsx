88502220252cf7aa531287f0f96f1e1de90305b6
Filipe Cândido <filipecandido123@hotmail.com>
2022-09-21 01:14:53 -0300

    feat: Ensure SignUp implement path Link to Login

--- src/pages/login/Signup.tsx
+++ src/pages/login/Signup.tsx
@@ -1,5 +1,6 @@
 import Input from 'components/input/Input';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function SingUp(): JSX.Element {
   return (
@@ -7,6 +8,10 @@ export function SingUp(): JSX.Element {
       action='#'
       className='flex flex-col h-screen justify-center items-center'
     >
+      <div className='android:relative android:bottom-20'>
+        <h1 className='font-serif text-4xl'>Lojão Popular</h1>
+      </div>
+      <span className='text-base '>Crie sua nova conta</span>
       <Input
         label={'Nome'}
         type={'text'}
@@ -55,6 +60,18 @@ export function SingUp(): JSX.Element {
           null;
         }}
       />
+      <button
+        type='button'
+        className='android:relative android:top-32 android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+      >
+        Registrar
+      </button>
+      <span className='android:relative android:top-36'>
+        Já tem uma conta?{' '}
+        <Link to='/login'>
+          <b className='underline'>Login</b>
+        </Link>
+      </span>
     </form>
   );
 }
