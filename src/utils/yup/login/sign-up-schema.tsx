b501c96674a32b69220979aee931fc130fb8f31b
Filipe Cândido <filipecandido123@hotmail.com>
2022-09-22 16:49:36 -0300

    feat: create schema signUp

---
+++ src/utils/yup/login/sign-up-schema.tsx
@@ -0,0 +1,18 @@
+import * as Yup from 'yup';
+
+const signUpSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(4, 'O nome é muito curto - deve ser 4 caracteres mínimo')
+    .required('Nome requerido'),
+  email: Yup.string()
+    .email('Email deve ser um email válido')
+    .required('Email requerido'),
+  password: Yup.string()
+    .required('Senha requerida')
+    .min(8, 'A senha é muito curta - deve ser 8 caracteres mínimo'),
+  passwordConfirmation: Yup.string()
+    .min(8, 'A senha é muito curta - deve ser 8 caracteres mínimo')
+    .required('Confirmação da senha requerido'),
+});
+
+export default signUpSchema;
