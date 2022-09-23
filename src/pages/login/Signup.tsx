3eb0c90b87194b8abfeb4d1a229514455272e93b
Filipe Cândido <filipecandido123@hotmail.com>
2022-09-22 23:30:40 -0300

    feat: add validations in sign Up

--- src/pages/login/Signup.tsx
+++ src/pages/login/Signup.tsx
@@ -1,77 +1,167 @@
-import Input from 'components/input/Input';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import signUpSchema from 'utils/yup/login/sign-up-schema';
+
+type PropsRegister = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+};
 
 export function SingUp(): JSX.Element {
+  const [name] = useState<string>('');
+  const [email] = useState<string>('');
+  const [password] = useState<string>('');
+  const [passwordConfirmation] = useState<string>('');
+
+  const registerValues: PropsRegister = {
+    name,
+    email,
+    password,
+    passwordConfirmation,
+  };
+
+  // const register = async (): Promise<any> => {
+  //   // eslint-disable-next-line no-console
+  //   console.log(values);
+  // };
+
   return (
-    <form
-      action='#'
-      className='flex flex-col h-screen justify-center items-center'
+    <Formik
+      initialValues={registerValues}
+      validationSchema={signUpSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
     >
-      <div className='android:relative android:bottom-20'>
-        <h1 className='font-serif text-4xl'>Lojão Popular</h1>
-      </div>
-      <span className='text-base '>Crie sua nova conta</span>
-      <Input
-        label={'Nome'}
-        type={'text'}
-        name={'name'}
-        className={
-          'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        }
-        testId={'name'}
-        onChange={(): void => {
-          null;
-        }}
-      />
-      <Input
-        label={'Email'}
-        type={'email'}
-        name={'email'}
-        className={
-          'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        }
-        testId={'email'}
-        onChange={(): void => {
-          null;
-        }}
-      />
-      <Input
-        type={'password'}
-        name={'password'}
-        className={
-          'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        }
-        label={'Senha'}
-        testId={'password'}
-        onChange={(): void => {
-          null;
-        }}
-      />
-      <Input
-        label={'Confirme sua senha'}
-        type={'password'}
-        name={'passwordConfirmation'}
-        className={
-          'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        }
-        testId={'password-confirmation'}
-        onChange={(): void => {
-          null;
-        }}
-      />
-      <button
-        type='button'
-        className='android:relative android:top-32 android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-      >
-        Registrar
-      </button>
-      <span className='android:relative android:top-36'>
-        Já tem uma conta?{' '}
-        <Link to='/login'>
-          <b className='underline'>Login</b>
-        </Link>
-      </span>
-    </form>
+      {(formik) => {
+        const { errors, dirty, isValid, touched } = formik;
+        return (
+          <Form>
+            <div className='flex flex-col h-screen justify-center items-center'>
+              <div className='android:relative android:bottom-20'>
+                <h1 className='text-4xl'>Lojão Popular</h1>
+              </div>
+              <span className='text-base'>Crie sua nova conta</span>
+              <div className='mb-2'>
+                <label
+                  htmlFor='name'
+                  className='block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Nome
+                </label>
+                <Field
+                  type='text'
+                  name='name'
+                  id='name'
+                  className={
+                    errors.email && touched.email
+                      ? 'android:w-80 block w-full p-2.5 border border-red-800 text-sm rounded-lg'
+                      : 'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  }
+                  data-testid='input-name'
+                />
+                <ErrorMessage
+                  name='name'
+                  component='span'
+                  className='text-xs text-red-900 mt-1'
+                />
+              </div>
+              <div className='mb-2'>
+                <label
+                  htmlFor='name'
+                  className='block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Email
+                </label>
+                <Field
+                  type='email'
+                  name='email'
+                  id='email'
+                  className={
+                    errors.email && touched.email
+                      ? 'android:w-80 block w-full p-2.5 border border-red-800 text-sm rounded-lg'
+                      : 'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  }
+                  data-testid='input-email'
+                />
+                <ErrorMessage
+                  name='email'
+                  component='span'
+                  className='text-xs text-red-900 h-0'
+                />
+              </div>
+              <div className='mb-2'>
+                <label
+                  htmlFor='name'
+                  className='block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Senha
+                </label>
+                <Field
+                  type='password'
+                  name='password'
+                  id='password'
+                  className={
+                    errors.email && touched.email
+                      ? 'android:w-80 block w-full p-2.5 border border-red-800 text-sm rounded-lg'
+                      : 'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  }
+                  data-testid='input-password'
+                />
+                <ErrorMessage
+                  name='password'
+                  component='span'
+                  className='text-xs text-red-900 mt-1'
+                />
+              </div>
+              <div className='mb-2'>
+                <label
+                  htmlFor='name'
+                  className='block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300'
+                >
+                  Confirme sua senha
+                </label>
+                <Field
+                  type='password'
+                  name='passwordConfirmation'
+                  id='passwordConfirmation'
+                  className={
+                    errors.email && touched.email
+                      ? 'android:w-80 block w-full p-2.5 border border-red-800 text-sm rounded-lg'
+                      : 'android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  }
+                  data-testid='input-password-confirmation'
+                />
+                <ErrorMessage
+                  name='passwordConfirmation'
+                  component='span'
+                  className='text-xs text-red-900 mt-1'
+                />
+              </div>
+              <button
+                type='submit'
+                className={
+                  !(dirty && isValid)
+                    ? 'android:relative android:top-32 disabled:opacity-30  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    : 'android:relative android:top-32 android:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                }
+                disabled={!(dirty && isValid)}
+              >
+                Registrar
+              </button>
+              <span className='android:relative android:top-36'>
+                Já tem uma conta?{' '}
+                <Link to='/login'>
+                  <b className='underline'>Login</b>
+                </Link>
+              </span>
+            </div>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
