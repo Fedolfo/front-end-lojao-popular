094b2c9554f982d8ebb20550f3d6df676e6391e1
Filipe Cândido <filipecandido123@hotmail.com>
2022-09-28 18:23:55 -0300

    feat: add Header in component Home and list navigation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,19 @@
+import Header from 'components/header/Header';
 import React from 'react';
 
 function Home(): JSX.Element {
   return (
     <div>
+      <div className='bg-red-500'>
+        <Header />
+        <nav>
+          <ol className='flex space-x-10 bg-red-600 text-white text-shadow justify-center font-LexendDeca text-lg'>
+            <li>Categorias</li>
+            <li>Localização</li>
+            <li>Contato</li>
+          </ol>
+        </nav>
+      </div>
       <p>Hello Home</p>
     </div>
   );
