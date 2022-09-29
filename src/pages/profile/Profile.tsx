5093e41d5d4d87b3c02a7130b6847439091c835f
Filipe Cândido <filipecandido123@hotmail.com>
2022-09-29 16:35:53 -0300

    refactor: Component Profile render navegation using map

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -3,41 +3,35 @@ import iconCart from '../../assets/images/iconShoppingCart.png';
 import iconSetting from '../../assets/images/iconSettingsOutline.png';
 import iconMessage from '../../assets/images/iconMessageCircleOutline.png';
 import iconHome from '../../assets/images/iconHomeOutline.png';
-// import iconClose from '../../assets/images/iconCloseOutline.png';
 import iconAlert from '../../assets/images/iconAlertTriangleOutline.png';
+import { Link } from 'react-router-dom';
 
 function Profile(): JSX.Element {
   return (
-    <>
-      <div>
-        <p>nome</p>
-        <p>email</p>
+    <div className='bg-[#D9D9D9]'>
+      <div className='flex flex-col font-LexendDeca android:text-2xl android:mt-20 android:ml-5'>
+        <p className='mb-1'>Filipe Cândido</p>
+        <p>filipe@email.com</p>
       </div>
-      <nav>
-        <ol>
+      <nav className='android:mt-36'>
+        <ol className='flex flex-col font-LexendDeca android:text-2xl android:ml-5'>
           <li>
-            <img src={iconHome} alt='Icone home' />
-            Inicio
-          </li>
-          <li>
-            <img src={iconCart} alt='Carrinho de compras' />
-            Carrinho
-          </li>
-          <li>
-            <img src={iconMessage} alt='Contato' />
-            Contato
-          </li>
-          <li>
-            <img src={iconAlert} alt='Icone alerta' />
-            Ajuda & suporte
-          </li>
-          <li>
-            <img src={iconSetting} alt='Configurações' />
-            Configurações
+            {[
+              ['Inicio', '/', iconHome],
+              ['Carrinho', '/cart', iconCart],
+              ['Contato', '/contact', iconMessage],
+              ['Ajuda & suporte', '/support', iconAlert],
+              ['Configurações', '/profile/configuration', iconSetting],
+            ].map(([title, url, images]) => (
+              <Link className='flex items-center' to={url} key={title}>
+                <img src={images} alt='Icone home' className='mr-2' />
+                {title}
+              </Link>
+            ))}
           </li>
         </ol>
       </nav>
-    </>
+    </div>
   );
 }
 
