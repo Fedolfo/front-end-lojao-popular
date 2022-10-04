90edaf4a1a5b88bdc5ab5285d7ce395c44842083
Filipe Cândido <filipecandido123@hotmail.com>
2022-10-04 00:15:27 -0300

    fix: mockjson price change string for number

--- src/service/mockjson.tsx
+++ src/service/mockjson.tsx
@@ -4,34 +4,34 @@ export const data = [
     title: 'Carrinho de bebê Carrinho de bebê',
     image:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80',
-    price: '20,30',
+    price: 30.8,
   },
   {
     id: 2,
     title: 'Carrinho de bebê',
     image:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80',
-    price: '20,30',
+    price: 30.8,
   },
   {
     id: 3,
     title: 'Carrinho de bebê',
     image:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80',
-    price: '20,30',
+    price: 30.8,
   },
   {
     id: 4,
     title: 'Carrinho de bebê',
     image:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80',
-    price: '20,30',
+    price: 30.8,
   },
   {
     id: 5,
     title: 'Carrinho de bebê',
     image:
       'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80',
-    price: '20,30',
+    price: 30.8,
   },
 ];
