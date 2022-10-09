7f893edb1ccd5da6af23df19180591e76442c189
Filipe Cândido <filipecandido123@hotmail.com>
2022-10-09 18:43:17 -0300

    feat: remove image the page NoMatch

--- src/pages/nomatch/NoMatch.tsx
+++ src/pages/nomatch/NoMatch.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import EmptyFish from '../../assets/images/emptyFish.jpg';
 
 function NoMatch(): JSX.Element {
   return (
@@ -11,7 +10,6 @@ function NoMatch(): JSX.Element {
           <p className='text-xl mt-4 animate-bounce'>
             Página não encontrada ¯\_(ツ)_/¯
           </p>
-          <img src={EmptyFish} alt='aquário' className='android:w-72' />
           <Link to='/' className='underline text-xl flex justify-center'>
             Voltar a pagina inícial
           </Link>
